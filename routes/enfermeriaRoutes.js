9e75f63ac1ac63bf63fa1fa104621eeaa5dcd7ba
Vitoan <119367979+Vitoan@users.noreply.github.com>
2026-02-25 23:30:17 -0300

    re start

--- routes/enfermeriaRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const enfermeriaController = require('../controllers/enfermeriaController');
-const { protegerRuta, verificarRol } = require('../middlewares/authMiddleware');
-
-// Solo los Enfermeros (y el Admin para pruebas) pueden registrar estas evaluaciones
-router.post('/evaluacion', protegerRuta, verificarRol('Enfermero', 'Admin'), enfermeriaController.registrarEvaluacion);
-
-module.exports = router;
--- routes/viewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Renderizar la pantalla de Login
-router.get('/login', (req, res) => {
-    res.render('login'); // Esto busca el archivo views/login.pug
-});
-
-module.exports = router;
